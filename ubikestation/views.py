2aa7d7c82cc8ab283d9f3a9cdc304cf3e402bced
muse83831 <muse83831@hotmail.com>
2017-07-21 16:32:29 +0800

    implemented isValidLatLng

--- ubikestation/views.py
+++ ubikestation/views.py
@@ -4,5 +4,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# given a latlng input return two nearest ubike stations
 def getTwoNearestStations(request):
-    return HttpResponse("hello world")
+
+    lat = float(request.GET.get('lat'))
+    lng = float(request.GET.get('lng'))
+    errorCode = 0
+    nearestStations = []
+
+    if not isValidLatLng(lat, lng):
+        errorCode = -1
+	
+    return HttpResponse(errorCode)
+
+#TODO
+# receive ubike station data from api
+def getStationData():
+    return 0
+
+# return true if input latlng is valid, else false
+def isValidLatLng(lat, lng):
+    return (lat<=90 and lat>=-90) and (lng<=180 and lng>=-180)
+
+#TODO
+# return true if input latlng is in Taipei City, else false
+def isInTaipeiCity(lat, lng):
+    return 0
+
+	
