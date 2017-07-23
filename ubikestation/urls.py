e4f3d5a8cfac5df258230685a74f289cf94e9eb0
muse83831 <muse83831@hotmail.com>
2017-07-23 13:45:11 +0800

    updated urls.py

--- ubikestation/urls.py
+++ ubikestation/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^ubike-station/taipei$', views.getTwoNearestStations, name='getTwoNearestStations')
+    url(r'^v1/ubike-station/taipei$', views.getTwoNearestStations, name='getTwoNearestStations')
 ]
